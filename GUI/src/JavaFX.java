c0f1f61d95207810dabd5c82d1c7153d2f052665
Nick Litterio <nicholas-litterio@uiowa.edu>
2018-11-30 13:47:33 -0600

    Copied over old stuff

---
+++ GUI/src/JavaFX.java
@@ -0,0 +1,55 @@
+//
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFX extends Application {
+
+    public static int whereInArray = 0;
+
+    public static void main(String[] args) {
+        System.out.print("Hello world..");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+
+    public void layout() {
+
+    }
+
+    public void J48_Tree() {
+        /**************
+         *
+          Where the classifier code will go
+         Going to read in the lines that the user puts into the GUI and then temporarily save them
+         If any feature is missing, then it will be filled in with the average value
+         Once the values are saved, we run it down the tree
+         Output how its classified and the percent
+
+         area_worst is column
+         concave points_worst is column
+         concavity_mean is column
+         area_se is column
+         texture_worst is column
+         texture_mean is column
+         radius_mean is column
+         texture_se is column
+         perimeter_se is column
+
+        **************/
+
+
+
+    }
+
+ }
+
